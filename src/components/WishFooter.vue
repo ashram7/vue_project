b831a65a09e2903ec46828f25e5113a7ab255001
ashram7 <ashram7@hanmail.net>
2021-12-13 16:48:26 +0900

    모두 삭제하기 버튼을 포함하는 WishFooter 컴포넌트

--- src/components/WishFooter.vue
+++ src/components/WishFooter.vue
@@ -1,10 +1,32 @@
 <!-- WishFooter.vue -->
 <template lang="html">
-    <div>footer</div>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" @click="clearWish">Clear All</span>
+  </div>
 </template>
 <script>
     export default {
+      methods: {
+        clearWish() {
+          localStorage.clear();
+        }
+      }
     }
 </script>
 <style lang="css" scoped>
+.clearAllContainer{
+    width: 7.5em;
+    height: 50px;
+    line-height: 50px;
+    background: linear-gradient(to right, #ff4c4d, #b30000);
+    border-radius: 5px;
+    margin: 25px auto 0px;
+}
+.clearAllBtn{
+    font-weight: bold;
+    text-transform: uppercase;
+    letter-spacing: -1px;
+    color: white;
+    display: block;
+}
 </style>
