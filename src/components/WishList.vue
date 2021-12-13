6c89da11d4f061474c196435e6963ed001ee0ca9
ashram7 <ashram7@hanmail.net>
2021-12-13 16:12:54 +0900

    위시리스트 목록 만들고, 로컬 스토리지 데이터를 뷰 데이터에 저장하기

--- src/components/WishList.vue
+++ src/components/WishList.vue
@@ -1,9 +1,29 @@
 <!-- WishList.vue -->
 <template lang="html">
-    <div>list</div>
+  <section>
+    <ul>
+      <li>위시리스트 1</li>
+      <li>위시리스트 2</li>
+      <li>위시리스트 3</li>
+      <li>위시리스트 4</li>
+      <li>위시리스트 5</li>
+    </ul>
+  </section>
 </template>
 <script>
     export default {
+      data() {
+        return {
+          wishItems: []
+        }
+      },
+      created() {
+        if (localStorage.length > 0) {
+          for (var i = 0; i < localStorage.length; i++) {
+            this.wishItems.push(localStorage.key(i));
+          }
+        }
+      }      
     }
 </script>
 <style lang="css" scoped>
