46ad3264d6eee9302321e41bdabf27447b4564b1
ashram7 <ashram7@hanmail.net>
2021-12-13 15:59:17 +0900

    위시리스트를 입력하는 WishInput 컴포넌트

--- src/components/WishInput.vue
+++ src/components/WishInput.vue
@@ -1,9 +1,23 @@
 <!-- WishInput.vue -->
 <template lang="html">
-    <div>input</div>
+    <div>
+      <input type="text" v-model="newWishItem">
+      <button v-on:click="addWish">추가</button>
+    </div>
 </template>
 <script>
     export default {
+      data(){
+        return {
+          newWishItem:''
+        }
+      },
+      methods:{
+        addWish(){
+          //console.log(this.newWishItem);
+          localStorage.setItem(this.newWishItem, this.newWishItem);
+        }
+      }
     }
 </script>
 <style lang="css" scoped>
