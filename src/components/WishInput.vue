01f115a7a6171b3cb3823964917dfa8847171eb8
ashram7 <ashram7@hanmail.net>
2021-12-13 16:06:07 +0900

    addWish() 안에 예외 처리 코드 넣기

--- src/components/WishInput.vue
+++ src/components/WishInput.vue
@@ -14,9 +14,20 @@
       },
       methods:{
         addWish(){
-          //console.log(this.newWishItem);
-          localStorage.setItem(this.newWishItem, this.newWishItem);
-        }
+          // 인풋 박스의 입력 값이 있을 때만 저장
+          if(this.newWishItem !== ""){
+            // 인풋 박스에 입력된 텍스트의 앞뒤 공백 문자열 제거
+            var value = this.newWishItem && this.newWishItem.trim();
+            //console.log(this.newWishItem);
+            //localStorage.setItem(this.newWishItem, this.newWishItem);
+            localStorage.setItem(value, value);
+            // 인풋 박스의 입력 값을 초기화
+            this.clearInput();
+          }
+        },
+        clearInput(){
+          this.newWishItem = '';
+        }        
       }
     }
 </script>
