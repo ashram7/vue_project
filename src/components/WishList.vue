005d489b2b01ba5afa9eadbb26e1c89b0393e8bf
ashram7 <ashram7@hanmail.net>
2021-12-13 16:29:15 +0900

    WishList.vue에 위시리스트 삭제 기능 추가하기

--- src/components/WishList.vue
+++ src/components/WishList.vue
@@ -2,7 +2,13 @@
 <template lang="html">
   <section>
     <ul>
-      <li v-for="wishItem in wishItems"> {{ wishItem }} </li>
+      <li v-for="(wishItem, index) in wishItems" :key="wishItem" class="shadow">
+        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+        {{ wishItem }}
+        <span class="removeBtn" type="button">
+          <i class="far fa-trash-alt" aria-hidden="true" @click="removeWish"></i>
+        </span>
+      </li>
     </ul>
   </section>
 </template>
@@ -11,6 +17,12 @@
       data() {
         return {
           wishItems: []
+        }
+      },
+      methods: {
+        removeWish(wishItem, index) {
+          localStorage.removeItem(wishItem);
+          this.wishItems.splice(index, 1);
         }
       },
       created() {
@@ -23,4 +35,31 @@
     }
 </script>
 <style lang="css" scoped>
+ul{
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0px;
+    text-align: left;
+}
+li{
+    display: flex;  // 비율 기준의 레이아웃 방식인 flex로 지정
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5em 0;
+    padding: 0 0.9em;
+    background: #fefefe;
+    border-radius: 5px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+.checkBtn{
+    line-height: 50px;
+    color: #ff8000;
+    margin-right: 5px;
+}
+.removeBtn{
+    margin-left: auto;
+    color: #de4343;
+}
 </style>
